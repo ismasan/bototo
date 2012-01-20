032d43b84f682d54688c47dbd96ab866677905fd
Ismael Celis <ismaelct@gmail.com>
2012-01-20 19:40:03 +0000

    Maps handler

---
+++ lib/bototo/handlers/maps_handler.rb
@@ -0,0 +1,19 @@
+Bototo.register do  
+  
+  URL = 'http://maps.google.com/maps/api/staticmap?markers='
+
+  on 'map me' do |value|      
+    if value =~ /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
+      say url
+    else
+      location = CGI.escape(value)
+      map_url = URL + "#{location}&size=400x400&maptype=roadmap&sensor=false&format=png"
+      link_url = "http://maps.google.com/maps?q=#{location}&hl=en&sll=37.0625,-95.677068&sspn=73.579623,100.371094&vpsrc=0&hnear=#{location}&t=m&z=11"
+      say(map_url) {
+        say link_url
+      }
+    end
+  end
+  
+end
+
